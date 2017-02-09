e0e7abff717023f67d0ab88bf038cb4cca823e33
Jordan Lewis <jordanthelewis@gmail.com>
2017-02-08 21:37:56 -0500

    Add order route, model, controller

---
+++ ruby/activerecord/app/models/order.rb
@@ -0,0 +1,5 @@
+class Order < ActiveRecord::Base
+  belongs_to :customer
+
+  has_and_belongs_to_many :products, join_table: 'order_products'
+end
--- ruby/activerecord/config/routes.rb
+++ ruby/activerecord/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   resources :customers, path: '/customer'
   resources :products, path: '/product'
+  resources :orders, path: '/order'
 end
