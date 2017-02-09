ee9094a4c1955ac3865bc8e138162ff976c4a19c
Jordan Lewis <jordanthelewis@gmail.com>
2017-02-09 15:48:52 -0500

    Route for add_product_to_order
    And remove unused routes from other resources

--- ruby/activerecord/config/routes.rb
+++ ruby/activerecord/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get :ping, to: 'ping#ping'
 
-  resources :customers, path: '/customer'
-  resources :products, path: '/product'
-  resources :orders, path: '/order'
+  resources :customers, path: '/customer', except: [:edit, :new]
+  resources :products,  path: '/product',  except: [:edit, :new]
+  resources :orders,    path: '/order',    except: [:edit, :new] do
+    post 'product', to: 'products#add_product_to_order'
+  end
 end
