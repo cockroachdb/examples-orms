7202f90bac153ce0f020f2fef8ca8b89c9fea689
Cuong Do <cdo@cockroachlabs.com>
2017-07-06 16:02:54 -0400

    Fix Sequelize 4.x compatiblity
    Sequelize 4.x significantly changes its models. Among other things,
    models are now full ES6 classes (before they were some object that
    wasn't a class). Because of this, adding an instance method for
    Sequelize 3.x objects is different from Sequelize 4.x.
    
    The easiest way to resolve this was to convert the instance methods into
    helper functions.
    
    Tested with both Sequelize 3.30.2 and 4.2.1.

--- node/sequelize/models/index.js
+++ node/sequelize/models/index.js
@@ -16,38 +16,32 @@ if (process.env.ADDR === undefined) {
 module.exports.Customer = sequelize.define('customer', {
   name: DataTypes.STRING
 }, {
-  instanceMethods: {
-    // jsonObject returns an object that JSON.stringify can trivially
-    // convert into the JSON response the test driver expects.
-    jsonObject: function() {
-      return {
-        id: parseInt(this.id),
-        name: this.name
-      };
-    }
-  },
   timestamps: false
 });
+
+module.exports.customerToJSON = function(customer) {
+  return {
+    id: parseInt(this.id),
+    name: this.name
+  };
+};
 
 module.exports.Order = sequelize.define('order', {
   customer_id: DataTypes.INTEGER,
   subtotal: DataTypes.DECIMAL(18, 2)
 }, {
-  instanceMethods: {
-    // jsonObject returns an object that JSON.stringify can trivially
-    // convert into the JSON response the test driver expects.
-    jsonObject: function() {
-      return{
-        id: parseInt(this.id),
-        subtotal: this.subtotal,
-        customer: {
-          id: this.customer_id
-        }
-      };
-    }
-  },
   timestamps: false
 });
+
+module.exports.orderToJSON = function(order) {
+  return{
+    id: parseInt(order.id),
+    subtotal: order.subtotal,
+    customer: {
+      id: order.customer_id
+    }
+  };
+};
 
 module.exports.OrderProduct = sequelize.define('order_products', {
   order_id: {
@@ -66,19 +60,16 @@ module.exports.Product = sequelize.define('product', {
   name: DataTypes.STRING,
   price: DataTypes.DECIMAL(18, 2)
 }, {
-  instanceMethods: {
-    // jsonObject returns an object that JSON.stringify can trivially
-    // convert into the JSON response the test driver expects.
-    jsonObject: function() {
-      return {
-        id: parseInt(this.id),
-        name: this.name,
-        price: this.price,
-      };
-    }
-  },
   timestamps: false
 });
+
+module.exports.productToJSON = function(product) {
+  return {
+    id: parseInt(product.id),
+    name: product.name,
+    price: product.price,
+  };
+};
 
 module.exports.sequelize = sequelize;
 module.exports.Sequelize = Sequelize;
