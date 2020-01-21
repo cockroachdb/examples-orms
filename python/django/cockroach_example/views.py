e2188137226cc5beea265bfe800b1686c6905ed4
Adam Pantel <adam@cockroachlabs.com>
2020-01-21 16:28:17 -0500

    Add retry decorator to django

--- python/django/cockroach_example/views.py
+++ python/django/cockroach_example/views.py
@@ -2,15 +2,33 @@ from django.http import JsonResponse, HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.generic import View
 from django.views.decorators.csrf import csrf_exempt
+from django.db import IntegrityError
 
 import json
 import sys
+import time
 
 from .models import *
 
 class PingView(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse("python/django", status=200)
+
+def retry_on_exception(num_retries=3, on_failure=HttpResponse(status=500), delay_=0.5, backoff_=1.5):
+    def retry(view):
+        def wrapper(*args, **kwargs):
+            delay = delay_
+            for i in range(num_retries):
+                try:
+                    return view(*args, **kwargs)
+                except IntegrityError as e:
+                    if i < num_retries - 1:
+                        time.sleep(delay)
+                        delay *= backoff_
+                    else:
+                        return on_failure
+        return wrapper
+    return retry
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CustomersView(View):
@@ -21,6 +39,7 @@ class CustomersView(View):
             customers = list(Customers.objects.filter(id=id).values())
         return JsonResponse(customers, safe=False)
 
+    @retry_on_exception(3)
     def post(self, request, *args, **kwargs):
         form_data = json.loads(request.body.decode())
         name = form_data['name']
@@ -28,6 +47,7 @@ class CustomersView(View):
         c.save()
         return HttpResponse(status=200)
 
+    @retry_on_exception(3)
     def delete(self, request, id=None, *args, **kwargs):
         if id is None:
             return HttpResponse(status=404)
@@ -46,6 +66,7 @@ class ProductView(View):
             products = list(Products.objects.filter(id=id).values())
         return JsonResponse(products, safe=False)
 
+    @retry_on_exception(3)
     def post(self, request, *args, **kwargs):
         form_data = json.loads(request.body.decode())
         name, price = form_data['name'], form_data['price']
@@ -65,6 +86,7 @@ class OrdersView(View):
             orders = list(Orders.objects.filter(id=id).values())
         return JsonResponse(orders, safe=False)
     
+    @retry_on_exception(3)
     def post(self, request, *args, **kwargs):
         form_data = json.loads(request.body.decode())
         c = Customers.objects.get(id=form_data['customer']['id'])
