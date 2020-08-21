2b4bcbaaf4ab146813aaa1272a97d70cc8545ff7
Tobias Grieger <tobias.b.grieger@gmail.com>
2020-08-21 11:20:07 +0200

    Revert "Make all tests run in insecure mode"
    This reverts commit 4e2c6a50add3b69c1e87d398550eecdcf0ea4e95.
    
    The preceding commit makes this work out.
    
    Closes https://github.com/cockroachdb/examples-orms/issues/117.

--- testing/main_test.go
+++ testing/main_test.go
@@ -351,12 +351,7 @@ func testORM(t *testing.T, info testInfo) {
 }
 
 func TestGORM(t *testing.T) {
-	testORM(t, testInfo{
-		language: "go",
-		orm:      "gorm",
-		// TODO(rafi): Test secure mode. See https://github.com/cockroachdb/examples-orms/issues/117)
-		insecure: true,
-	})
+	testORM(t, testInfo{language: "go", orm: "gorm"})
 }
 
 func TestGOPG(t *testing.T) {
@@ -396,8 +391,6 @@ func TestSQLAlchemy(t *testing.T) {
 	testORM(t, testInfo{
 		language: "python",
 		orm:      "sqlalchemy",
-		// TODO(rafi): Test secure mode. See https://github.com/cockroachdb/examples-orms/issues/117)
-		insecure: true,
 	})
 }
 
@@ -414,19 +407,9 @@ func TestDjango(t *testing.T) {
 }
 
 func TestActiveRecord(t *testing.T) {
-	testORM(t, testInfo{
-		language: "ruby",
-		orm:      "activerecord",
-		// TODO(rafi): Test secure mode. See https://github.com/cockroachdb/examples-orms/issues/117)
-		insecure: true,
-	})
+	testORM(t, testInfo{language: "ruby", orm: "activerecord"})
 }
 
 func TestActiveRecord4(t *testing.T) {
-	testORM(t, testInfo{
-		language: "ruby",
-		orm:      "ar4",
-		// TODO(rafi): Test secure mode. See https://github.com/cockroachdb/examples-orms/issues/117)
-		insecure: true,
-	})
+	testORM(t, testInfo{language: "ruby", orm: "ar4"})
 }
