0c53b556afb54e12316ebbd0969e0598a6472a95
Eric Harmeling <eric.harmeling@cockroachlabs.com>
2020-11-10 16:51:41 -0500

    Updated view retry function control flow, error exceptions

--- python/django/cockroach_example/views.py
+++ python/django/cockroach_example/views.py
@@ -2,9 +2,10 @@ from django.http import JsonResponse, HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.generic import View
 from django.views.decorators.csrf import csrf_exempt
-from django.db import Error, IntegrityError
+from django.db import Error, OperationalError
 from django.db.transaction import atomic
-
+from psycopg2 import errorcodes
+from functools import wraps
 import json
 import sys
 import time
@@ -12,22 +13,23 @@ import time
 from .models import *
 
 # Warning: Do not use retry_on_exception in an inner nested transaction.
-def retry_on_exception(num_retries=3, on_failure=HttpResponse(status=500), delay_=0.5, backoff_=1.5):
-    def retry(view):
-        def wrapper(*args, **kwargs):
-            delay = delay_
-            for i in range(num_retries):
-                try:
-                    return view(*args, **kwargs)
-                except IntegrityError as ex:
-                    if i == num_retries - 1:
-                        return on_failure
-                    elif getattr(ex.__cause__, 'pgcode', '') == errorcodes.SERIALIZATION_FAILURE:
-                        time.sleep(delay)
-                        delay *= backoff_
-                except Error as ex:
+def retry_on_exception(view, num_retries=3, on_failure=HttpResponse(status=500), delay_=0.5, backoff_=1.5):
+    @wraps(view)
+    def retry(*args, **kwargs):
+        delay = delay_
+        for i in range(num_retries):
+            try:
+                return view(*args, **kwargs)
+            except OperationalError as ex:
+                if i == num_retries - 1:
                     return on_failure
-        return wrapper
+                elif getattr(ex.__cause__, 'pgcode', '') == errorcodes.SERIALIZATION_FAILURE:
+                    time.sleep(delay)
+                    delay *= backoff_
+                else:
+                    return on_failure
+            except Error as ex:
+                return on_failure
     return retry
 
 class PingView(View):
