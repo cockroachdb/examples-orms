f4b2bdbf87edc592a214cd9d06d6e79a69a90ef0
Eric Harmeling <eric.harmeling@cockroachlabs.com>
2020-11-16 16:39:49 -0500

    Updated view retry function control flow, error exceptions (#123)
    * Updated view retry function control flow, error exceptions
    
    * Fix retry decorator calls

--- python/django/cockroach_example/views.py
+++ python/django/cockroach_example/views.py
@@ -2,9 +2,10 @@ from django.http import JsonResponse, HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.generic import View
 from django.views.decorators.csrf import csrf_exempt
-from django.db import Error, IntegrityError
+from django.db import Error, OperationalError
 from django.db.transaction import atomic
-
+from psycopg2 import errorcodes
+from functools import wraps
 import json
 import sys
 import time
@@ -12,22 +13,23 @@ import time
 from .models import *
 
 # Warning: Do not use retry_on_exception in an inner nested transaction.
-def retry_on_exception(num_retries=3, on_failure=HttpResponse(status=500), delay_=0.5, backoff_=1.5):
-    def retry(view):
-        def wrapper(*args, **kwargs):
-            delay = delay_
-            for i in range(num_retries):
-                try:
-                    return view(*args, **kwargs)
-                except IntegrityError as ex:
-                    if i == num_retries - 1:
-                        return on_failure
-                    elif getattr(ex.__cause__, 'pgcode', '') == errorcodes.SERIALIZATION_FAILURE:
-                        time.sleep(delay)
-                        delay *= backoff_
-                except Error as ex:
+def retry_on_exception(view, num_retries=3, on_failure=HttpResponse(status=500), delay_=0.5, backoff_=1.5):
+    @wraps(view)
+    def retry(*args, **kwargs):
+        delay = delay_
+        for i in range(num_retries):
+            try:
+                return view(*args, **kwargs)
+            except OperationalError as ex:
+                if i == num_retries - 1:
                     return on_failure
-        return wrapper
+                elif getattr(ex.__cause__, 'pgcode', '') == errorcodes.SERIALIZATION_FAILURE:
+                    time.sleep(delay)
+                    delay *= backoff_
+                else:
+                    return on_failure
+            except Error as ex:
+                return on_failure
     return retry
 
 class PingView(View):
@@ -43,7 +45,7 @@ class CustomersView(View):
             customers = list(Customers.objects.filter(id=id).values())
         return JsonResponse(customers, safe=False)
 
-    @retry_on_exception(3)
+    @retry_on_exception
     @atomic
     def post(self, request, *args, **kwargs):
         form_data = json.loads(request.body.decode())
@@ -52,7 +54,7 @@ class CustomersView(View):
         c.save()
         return HttpResponse(status=200)
 
-    @retry_on_exception(3)
+    @retry_on_exception
     @atomic
     def delete(self, request, id=None, *args, **kwargs):
         if id is None:
@@ -72,7 +74,7 @@ class ProductView(View):
             products = list(Products.objects.filter(id=id).values())
         return JsonResponse(products, safe=False)
 
-    @retry_on_exception(3)
+    @retry_on_exception
     @atomic
     def post(self, request, *args, **kwargs):
         form_data = json.loads(request.body.decode())
@@ -93,7 +95,7 @@ class OrdersView(View):
             orders = list(Orders.objects.filter(id=id).values())
         return JsonResponse(orders, safe=False)
     
-    @retry_on_exception(3)
+    @retry_on_exception
     @atomic
     def post(self, request, *args, **kwargs):
         form_data = json.loads(request.body.decode())
