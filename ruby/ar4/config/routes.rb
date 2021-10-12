e0ac48dc97843a786bc5d57d4ef1f8c5e84213a1
Rafi Shamim <rafi@cockroachlabs.com>
2021-10-11 23:34:05 -0400

    Remove ActiveRecord 4 testing

--- ruby/ar4/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get :ping, to: 'ping#ping'
-
-  resources :customers, path: '/customer', except: [:edit, :new]
-  resources :products,  path: '/product',  except: [:edit, :new]
-  resources :orders,    path: '/order',    except: [:edit, :new] do
-    post 'product', to: 'order_products#create'
-  end
-end
