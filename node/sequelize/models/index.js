e5c47a74c7ec0d0219eb36cdf8885f5826a65bf5
Rafi Shamim <rafi@cockroachlabs.com>
2021-11-18 14:35:53 -0500

    Simplify sequelize connection setup
    Sequelize 6.11.0 includes a change that allows us to just use the
    connection string rather than having to parse certs and configure the
    database name.

--- node/sequelize/models/index.js
+++ node/sequelize/models/index.js
@@ -1,48 +1,14 @@
 'use strict';
 
-var fs        = require('fs');
 var Sequelize = require('sequelize-cockroachdb');
 
 if (process.env.ADDR === undefined) {
   throw new Error("ADDR (database URL) must be specified.");
 }
 
-var url = new URL(process.env.ADDR);
-var opts = {
-  dialect: "postgres",
-  username: url.username,
-  host: url.hostname,
-  port: url.port,
-  database: url.pathname.substring(1), // ignore leading '/'
-  dialectOptions: {
-    cockroachdbTelemetryDisabled: true,
-    ssl: {},
-  },
-  logging: false,
-};
-
-if (url.password) {
-  opts.password = url.password
-}
-if (url.searchParams.has("options")) {
-  var pgOpts = url.searchParams.get("options")
-  var cluster = pgOpts.match(/cluster=([^\s]+)/)[1]
-  opts.database = `${cluster}.${opts.database}`
-}
-if (url.searchParams.get("sslmode") === "disable") {
-  delete opts.dialectOptions.ssl
-} else {
-  if (url.searchParams.has("sslrootcert")) {
-    opts.dialectOptions.ssl.ca = fs.readFileSync(url.searchParams.get("sslrootcert").toString())
-  }
-  if (url.searchParams.has("sslcert")) {
-    opts.dialectOptions.ssl.cert = fs.readFileSync(url.searchParams.get("sslcert").toString())
-  }
-  if (url.searchParams.has("sslkey")) {
-    opts.dialectOptions.ssl.key = fs.readFileSync(url.searchParams.get("sslkey").toString())
-  }
-}
-var sequelize = new Sequelize(opts);
+var sequelize = new Sequelize(process.env.ADDR, {
+  dialectOptions: {cockroachdbTelemetryDisabled : true}
+});
 var DataTypes = Sequelize.DataTypes;
 
 if (!Sequelize.supportsCockroachDB) {
